764e3ac1452276e1670c504122b37f4876ea8cbf
ejima <m46sque96rd@gmail.com>
2026-02-16 11:18:17 +0900

    🐛 Fix: サイトURL、エイリアス設定を修正

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,30 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+import { loadEnv } from 'vite';
 import * as config from './config.ts';
+
+let site = 'https://example.com/';
+const env = loadEnv(import.meta.env.MODE, process.cwd(), '');
+const { SITE_URL } = env;
+if (SITE_URL) {
+  site = SITE_URL.endsWith('/') ? SITE_URL : `${SITE_URL}/`;
+}
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com/',
+  site: site,
   base: config.pathPrefix,
   integrations: [icon(), sitemap()],
   vite: {
     define: {
       'import.meta.vitest': 'undefined',
+    },
+    resolve: {
+      alias: {
+        '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
     },
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     plugins: [tailwindcss()] as any,
